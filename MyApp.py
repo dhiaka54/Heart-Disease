f1f01980f4cf22d45badf12862df58cecb6af72d
dhiaka54 <111202013094@mhs.dinus.ac.id>
2024-01-08 06:08:17 +0700

    update

--- MyApp.py
+++ MyApp.py
@@ -86,11 +86,12 @@ y = df_clean['target']
 smote = SMOTE(random_state=42)
 X, y = smote.fit_resample(X, y)
 
-model = pickle.load(open("model/xgb_model.pkl", 'rb'))
+model = pickle.load(open("model/xgb_best_model.pkl", 'rb'))
 
 y_pred = model.predict(X)
+
 accuracy = accuracy_score(y, y_pred)
-accuracy = round((accuracy * 100), 2)
+accuracy = round((accuracy * 100), 3)
 
 df_final = X
 df_final['target'] = y
