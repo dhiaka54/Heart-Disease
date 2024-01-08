2ca44d58f07fda57742ec3b3e9389f45a232780f
Dhiaka Shabrina Assyifa <90568511+dhiaka54@users.noreply.github.com>
2024-01-08 07:25:39 +0700

    Update MyApp.py

--- MyApp.py
+++ MyApp.py
@@ -88,7 +88,7 @@ X_smote_resampled, y_smote_resampled = smote.fit_resample(X, y)
 
 model = pickle.load(open("model/xgb_best_model.pkl", 'rb'))
 
-y_pred_xgb = xgb_model.predict(X_test_normal)
+y_pred_xgb = model.predict(X_test_normal)
 accuracyxgb = round(accuracy_score(y_test_normal, y_pred_xgb),3)
 
 df_final = X
