be63786091703880c6adece0419b82020c59c587
Dhiaka Shabrina Assyifa <90568511+dhiaka54@users.noreply.github.com>
2024-01-08 07:48:17 +0700

    Update MyApp.py

--- MyApp.py
+++ MyApp.py
@@ -8,24 +8,6 @@ import time
 import pickle
 from sklearn.model_selection import train_test_split
 
-with open("data/hungarian.data", encoding='Latin1') as file:
-  lines = [line.strip() for line in file]
-
-data = itertools.takewhile(
-  lambda x: len(x) == 76,
-  (' '.join(lines[i:(i + 10)]).split() for i in range(0, len(lines), 10))
-)
-
-df = pd.DataFrame.from_records(data)
-
-df = df.iloc[:, :-1]
-df = df.drop(df.columns[0], axis=1)
-df = df.astype(float)
-
-df.replace(-9.0, np.NaN, inplace=True)
-
-df_selected = df.iloc[:, [1, 2, 7, 8, 10, 14, 17, 30, 36, 38, 39, 42, 49, 56]]
-
 column_mapping = {
   2: 'age',
   3: 'sex',
@@ -42,41 +24,12 @@ column_mapping = {
   50: 'thal',
   57: 'target'
 }
+df = pd.read_csv("data/hungarian.data", header=None)
 
-df_selected.rename(columns=column_mapping, inplace=True)
+df.rename(columns=column_mapping, inplace=True)
 
 columns_to_drop = ['ca', 'slope','thal']
-df_selected = df_selected.drop(columns_to_drop, axis=1)
-
-meanTBPS = df_selected['trestbps'].dropna()
-meanChol = df_selected['chol'].dropna()
-meanfbs = df_selected['fbs'].dropna()
-meanRestCG = df_selected['restecg'].dropna()
-meanthalach = df_selected['thalach'].dropna()
-meanexang = df_selected['exang'].dropna()
-
-meanTBPS = meanTBPS.astype(float)
-meanChol = meanChol.astype(float)
-meanfbs = meanfbs.astype(float)
-meanthalach = meanthalach.astype(float)
-meanexang = meanexang.astype(float)
-meanRestCG = meanRestCG.astype(float)
-
-meanTBPS = round(meanTBPS.mean())
-meanChol = round(meanChol.mean())
-meanfbs = round(meanfbs.mean())
-meanthalach = round(meanthalach.mean())
-meanexang = round(meanexang.mean())
-meanRestCG = round(meanRestCG.mean())
-
-fill_values = {
-  'trestbps': meanTBPS,
-  'chol': meanChol,
-  'fbs': meanfbs,
-  'thalach':meanthalach,
-  'exang':meanexang,
-  'restecg':meanRestCG
-}
+df = df.drop(columns_to_drop, axis=1)
 
 df_clean = df_selected.fillna(value=fill_values)
 df_clean.drop_duplicates(inplace=True)
